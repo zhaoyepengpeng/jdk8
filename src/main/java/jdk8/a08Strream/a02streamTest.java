9bc1be7adfbfaa939187649bb04b4b8a38a2b692
zhaoyepengpeng <1285872896@qq.com>
2023-04-11 06:27:43 +0800

    测试流

--- src/main/java/jdk8/a08Strream/a02streamTest.java
+++ src/main/java/jdk8/a08Strream/a02streamTest.java
@@ -1,5 +1,7 @@
 package jdk8.a08Strream;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class a02streamTest {
@@ -7,9 +9,11 @@ public class a02streamTest {
         IntStream.of(new int[]{5, 6, 7}).forEach(System.out::println);
         System.out.println("------------------------------------");
         //打印出来是34567，包头不包尾
-        IntStream.range(3, 8).forEach(System.out::println);
-        int sum = IntStream.range(1, 1000).sum();
-        System.out.println(sum);
-
+//        IntStream.range(3, 8).forEach(System.out::println);
+//        int sum = IntStream.range(1, 1000).sum();
+//        System.out.println(sum);
+        System.out.println("------------------------------------");
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        System.out.println(list.stream().map(i -> 2 * i).reduce(0,Integer::sum));
     }
 }
